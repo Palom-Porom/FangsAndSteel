58af884314b68d105363bbfb81c9bb52a2040341
Егор Деркунский <114829512+Palom-Porom@users.noreply.github.com>
2023-10-29 13:49:37 +0300

    Changed Team Component

--- FangsAndSteel/Assets/Scripts/General/TeamTags.cs
+++
@@ -1,7 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using Unity.Entities;
-using UnityEngine;
-
-public struct Team1Tag : IComponentData { }
-public struct Team2Tag : IComponentData { }
