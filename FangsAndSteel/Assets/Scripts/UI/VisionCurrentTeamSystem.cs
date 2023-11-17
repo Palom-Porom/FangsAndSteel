dfef00b896d98393b37e789ff110bb619591a2f8
Егор Деркунский <114829512+Palom-Porom@users.noreply.github.com>
2023-11-17 13:37:40 +0300

    Fixed VisionTeamSystem

--- FangsAndSteel/Assets/Scripts/UI/VisionCurrentTeamSystem.cs
+++ FangsAndSteel/Assets/Scripts/UI/VisionCurrentTeamSystem.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Drawing;
 using Unity.Entities;
 using Unity.Mathematics;
 using Unity.Transforms;
@@ -24,14 +25,19 @@ public partial struct VisionCurrentTeamSystem : ISystem
 
 public partial struct VisionCurrentTeamJob : IJobEntity
 {
-
     // in - чтение компонента, ref - чтение и запись (Возможность редактирования) компонента
     public DynamicBuffer<VisionMapBuffer>  visionMap;
+
+    const int ORIG_MAP_SIZE = 1000;
+    const int VIS_MAP_SIZE = 500;
+    const int ORIG_TO_VIS_MAP_RATIO = 2;
+
     public void Execute(in LocalToWorld localtoworld, in TeamComponent team, ref VisibilityComponent visibility)
     {
         float2 position;
-        position = localtoworld.Position.xz; //(x,z) -> k, int k = 4 перевод из двумерного массива позиций в одномерный массив позиций, в тот с помощью которого и работает у нас карта
-        int k = 0;
+        position = localtoworld.Position.xz + VIS_MAP_SIZE; 
+        //(x,z) -> k перевод из двумерного массива позиций в одномерный массив позиций, в тот с помощью которого и работает у нас карта
+        int k = (int)(position.x / ORIG_TO_VIS_MAP_RATIO + math.floor(position.y / ORIG_TO_VIS_MAP_RATIO) * VIS_MAP_SIZE);
         visibility.visibleToTeams = visionMap[k];
     }
 }
