661a6556ec968df8fe75970f1aba23906d3c37ef
AdmiralPikaso <itspodarok@mail.ru>
2023-11-18 22:03:24 +0300

    Create Unit Stats Window

--- FangsAndSteel/Assets/Scripts/UI/StaticUIRefs.cs
+++ FangsAndSteel/Assets/Scripts/UI/StaticUIRefs.cs
@@ -1,17 +1,37 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class StaticUIRefs : MonoBehaviour
 {
-    [SerializeField] public static StaticUIRefs Instance { get; private set; }
-    [SerializeField] public static StaticUIRefs hpText { get; }
-    [SerializeField] public static StaticUIRefs attackText { get; }
-    [SerializeField] public StaticUIRefs speedText { get; }
+    [HideInInspector] public static StaticUIRefs Instance { get; private set; }
+    [HideInInspector] public TextMeshProUGUI HpText { get { return hpText; } }
+    private TextMeshProUGUI hpText;
+    [HideInInspector] public TextMeshProUGUI AttackText { get { return attackText; } }
+    private TextMeshProUGUI attackText;
+    [HideInInspector] public TextMeshProUGUI ReloadText { get { return reloadText; } }
+    private TextMeshProUGUI reloadText;
+    [HideInInspector] public TextMeshProUGUI AttackRadiusText { get { return attackRadiusText; } }
+    private TextMeshProUGUI attackRadiusText;
+    [HideInInspector] public TextMeshProUGUI MovementText { get { return movementText; } }
+    private TextMeshProUGUI movementText;
+
+    public GameObject unitStats;
 
     void Awake()
     {
         Instance = this;
+    }
+
+    private void Start()
+    {
+        hpText = unitStats.transform.GetChild(0).GetComponent<TextMeshProUGUI>();
+        attackText = unitStats.transform.GetChild(1).GetComponent<TextMeshProUGUI>();
+        reloadText = unitStats.transform.GetChild(2).GetComponent<TextMeshProUGUI>();
+        attackRadiusText = unitStats.transform.GetChild(3).GetComponent<TextMeshProUGUI>();
+        movementText = unitStats.transform.GetChild(4).GetComponent<TextMeshProUGUI>();
     }
 
     [SerializeField] public bool endTurnBut;
--- FangsAndSteel/Assets/Scripts/UI/UnitStatsUiSystem.cs
+++ FangsAndSteel/Assets/Scripts/UI/UnitStatsUiSystem.cs
@@ -1,7 +1,9 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using TMPro.EditorUtilities;
 using Unity.Entities;
+using Unity.Mathematics;
 using UnityEngine;
 
 public partial class UnitStatsUiSystem : SystemBase
@@ -17,18 +19,17 @@ public partial class UnitStatsUiSystem : SystemBase
         movementStats = GetComponentLookup<MovementComponent>();
     }
 
-    bool initialFillDone;
-    StaticUIData uIData;
     protected override void OnUpdate()
     {
         hpStats.Update(this);
         attackStats.Update(this);
         movementStats.Update(this);
         Entity entity = SystemAPI.GetSingleton<UnitStatsRequestComponent>().entity;
-        if ( !initialFillDone )
-        {
-            StaticUIRefs.Instance.hptext
-        }
+        StaticUIRefs.Instance.HpText.text = $"HP: {hpStats[entity].curHp} / {hpStats[entity].maxHp}";
+        StaticUIRefs.Instance.AttackText.text = $"DMG: {attackStats[entity].damage}";
+        StaticUIRefs.Instance.ReloadText.text = $"ReloadTime: {attackStats[entity].curReload} / {attackStats[entity].reloadLen}";
+        StaticUIRefs.Instance.AttackRadiusText.text = $"AttackRadius: {math.sqrt(attackStats[entity].radiusSq)}";
+        StaticUIRefs.Instance.MovementText.text = $"Speed: {movementStats[entity].speed}";
     }
 }
 
