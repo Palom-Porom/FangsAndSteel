b553433556996aef837ac95c72dc5aa227ff66dc
Егор Деркунский <114829512+Palom-Porom@users.noreply.github.com>
2023-11-21 20:49:47 +0300

    Revert "Basic Menu and Buttons (#19)"
    This reverts commit 4e1d7093ab365a2daecae51eb3d87ade66c0906c.

--- FangsAndSteel/Assets/Scripts/Controls/Unit Selection/SelectionSystem.cs
+++ FangsAndSteel/Assets/Scripts/Controls/Unit Selection/SelectionSystem.cs
@@ -10,8 +10,6 @@ using Unity.Collections;
 using UnityEngine.InputSystem;
 using RaycastHit = Unity.Physics.RaycastHit;
 using Unity.Transforms;
-using UnityEngine.EventSystems;
-using Unity.VisualScripting;
 using Unity.Rendering;
 
 public partial class SelectionSystem : SystemBase
@@ -23,8 +21,6 @@ public partial class SelectionSystem : SystemBase
 
     private float2 mouseStartPos;
     private bool isDragging;
-    private bool wasClickedOnUI;
-    private const float SQRD_DISTANCE_TO_DRAG = 0.25f;
 
     private ComponentLookup<SelectTag> selectLookup;
 
@@ -61,16 +57,12 @@ public partial class SelectionSystem : SystemBase
     {
         if (Mouse.current.leftButton.wasPressedThisFrame)
         {
-            wasClickedOnUI = EventSystem.current.IsPointerOverGameObject();
-            if (!wasClickedOnUI)
-                mouseStartPos = Mouse.current.position.value;
+            mouseStartPos = Mouse.current.position.value;
         }
 
         else if (Mouse.current.leftButton.isPressed)
         {
-            if (!isDragging &&
-                math.distancesq(mouseStartPos, Mouse.current.position.value) > SQRD_DISTANCE_TO_DRAG &&
-                !wasClickedOnUI)
+            if (!isDragging && math.distancesq(mouseStartPos, Mouse.current.position.value) > SQRD_DISTANCE_TO_DRAG)
             {
                 isDragging = true;
                 GUI_Manager.Instance.isDragging = true;
@@ -80,12 +72,6 @@ public partial class SelectionSystem : SystemBase
 
         else if (Mouse.current.leftButton.wasReleasedThisFrame)
         {
-            //If was clicked on UI, then nothing to do
-            if (wasClickedOnUI)
-            {
-                wasClickedOnUI = false;
-                return;
-            }
             //Update containers
             selectLookup.Update(this);
             ecb = SystemAPI.GetSingleton<EndSimulationEntityCommandBufferSystem.Singleton>().CreateCommandBuffer(World.Unmanaged).AsParallelWriter();
--- FangsAndSteel/Assets/Scripts/UI/StaticUIRefs.cs
+++ FangsAndSteel/Assets/Scripts/UI/StaticUIRefs.cs
@@ -1,24 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
-using TMPro;
 
 public class StaticUIRefs : MonoBehaviour
 {
     [SerializeField] public static StaticUIRefs Instance { get; private set; }
-    [SerializeField] public Image TurnIndicator { get { return turnIndicator; } }
-    public Image turnIndicator;
-
-    [SerializeField] public TextMeshProUGUI TurnTimer { get { return turnTimer; } }
-    public TextMeshProUGUI turnTimer;
 
     void Awake()
     {
         Instance = this;
     }
 
-    [HideInInspector] public bool endTurnBut;
-    [HideInInspector] public bool stopMoveBut;
-    [HideInInspector] public bool changeSpeedBut;
+    [SerializeField] public bool endTurnBut;
+    [SerializeField] public bool stopMoveBut;
+    [SerializeField] public bool changeSpeedBut;
 }
