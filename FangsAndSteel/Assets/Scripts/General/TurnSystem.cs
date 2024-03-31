5395e817364d605997967cdb6135def8f0d232f6
Егор Деркунский <114829512+Palom-Porom@users.noreply.github.com>
2024-03-31 14:03:01 +0300

    Turn System Redone for Network
    I suppose? It can be some mistakes but it looks okay in general
    Still need alternative version of that System for the HotSeat mode

--- FangsAndSteel/Assets/Scripts/General/TurnSystem.cs
+++ FangsAndSteel/Assets/Scripts/General/TurnSystem.cs
@@ -3,22 +3,29 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.Entities;
 using Unity.Entities.UI;
+using Unity.NetCode;
 using UnityEngine;
 using UnityEngine.UI;
 
-[UpdateInGroup(typeof(SimulationSystemGroup), OrderLast = true)]
-public partial class TurnSystem : SystemBase
+public struct EndTurnRpc : IRpcCommand { }
+
+[WorldSystemFilter(WorldSystemFilterFlags.ServerSimulation)]
+public partial class _TurnSystem : SystemBase
 {
 
-    const float TURN_LEN = 10; 
-    StaticUIData uIData;
+    const uint NUM_OF_PLAYERS = 2;
+    List<int> readyPlayersInds;
+
+    const float TURN_LEN = 10;
+    //StaticUIData uIData;
     public static float timeToRun;
     bool orderPhase;
 
     protected override void OnCreate()
     {
-        RequireForUpdate<GameTag>();
-        RequireForUpdate<StaticUIData>();
+        //RequireForUpdate(*playerEntity*)
+        readyPlayersInds = new List<int>();
+
         timeToRun = 0;
         orderPhase = true;
     }
@@ -28,55 +35,134 @@ public partial class TurnSystem : SystemBase
         EnableEngageSystems(!orderPhase);
     }
 
-    public void OnStopRunning(ref SystemState state) 
+    public void OnStopRunning(ref SystemState state)
     {
-        orderPhase = !orderPhase;
+        orderPhase = !orderPhase; //Forgot for what was that?
     }
 
-    
     protected override void OnUpdate()
     {
-        //If Order phase
         if (orderPhase)
         {
-            uIData = SystemAPI.GetSingleton<StaticUIData>();
-            if (uIData.endTurnBut.IsSet)
+            foreach (var request in SystemAPI.Query<RefRO<ReceiveRpcCommandRequest>>().WithAll<EndTurnRpc>())
             {
-                //Start all Engage systems
-                EnableEngageSystems(true);
-                //Set Timer
-                timeToRun = TURN_LEN;
-
-                StaticUIRefs.Instance.TurnIndicator.color = Color.green;
-
-                orderPhase = false;
+                readyPlayersInds.Add(request.ValueRO.SourceConnection.Index);
+                if (readyPlayersInds.Count == NUM_OF_PLAYERS)
+                {
+                    StartEngageMode();
+                }
             }
         }
-        //If Engage phase
         else
         {
             timeToRun -= SystemAPI.Time.DeltaTime;
-            StaticUIRefs.Instance.TurnTimer.text = $"0:{(int)timeToRun:D2}";
+            //StaticUIRefs.Instance.TurnTimer.text = $"0:{(int)timeToRun:D2}"; <-- will be done on client
             if (timeToRun <= 0)
             {
-                StaticUIRefs.Instance.TurnTimer.text = "0:00";
-                //Stop all Engage systems
-                EnableEngageSystems(false);
-
-                StaticUIRefs.Instance.TurnIndicator.color = Color.red;
-
-                orderPhase = true;
+                //StaticUIRefs.Instance.TurnTimer.text = "0:00"; <-- will be done on client
+                StartOrderMode();
             }
         }
     }
 
-    private void EnableEngageSystems (bool enable)
+    private void StartEngageMode()
+    {
+        timeToRun = TURN_LEN;
+        EnableEngageSystems(true);
+        orderPhase = false;
+    }
+
+    private void StartOrderMode()
+    {
+        EnableEngageSystems(false);
+        orderPhase = true;
+        //StaticUIRefs.Instance.TurnIndicator.color = Color.red;  <-- will be done on client
+    }
+
+    private void EnableEngageSystems(bool enable)
     {
         World.Unmanaged.GetExistingSystemState<UnitsSystemGroup>().Enabled = enable;
         //World.Unmanaged.GetExistingSystemState<VisionMapSystem>().Enabled = enable;
         World.Unmanaged.GetExistingSystemState<AnimationSystem>().Enabled = enable;
 
-        World.Unmanaged.GetExistingSystemState<TargetingMoveSystem>().Enabled = !enable;
-        World.Unmanaged.GetExistingSystemState<BasicButtonSystem>().Enabled = !enable;
+        //Below will be done on the client after clicking EndTurnBut
+        //World.Unmanaged.GetExistingSystemState<TargetingMoveSystem>().Enabled = !enable;
+        //World.Unmanaged.GetExistingSystemState<BasicButtonSystem>().Enabled = !enable;
     }
 }
+
+
+//[UpdateInGroup(typeof(SimulationSystemGroup), OrderLast = true)]
+//public partial class TurnSystem : SystemBase
+//{
+
+//    const float TURN_LEN = 10; 
+//    StaticUIData uIData;
+//    public static float timeToRun;
+//    bool orderPhase;
+
+//    protected override void OnCreate()
+//    {
+//        RequireForUpdate<GameTag>();
+//        RequireForUpdate<StaticUIData>();
+//        timeToRun = 0;
+//        orderPhase = true;
+//    }
+
+//    protected override void OnStartRunning()
+//    {
+//        EnableEngageSystems(!orderPhase);
+//    }
+
+//    public void OnStopRunning(ref SystemState state) 
+//    {
+//        orderPhase = !orderPhase;
+//    }
+
+    
+//    protected override void OnUpdate()
+//    {
+//        //If Order phase
+//        if (orderPhase)
+//        {
+//            uIData = SystemAPI.GetSingleton<StaticUIData>();
+//            if (uIData.endTurnBut.IsSet)
+//            {
+//                //Start all Engage systems
+//                EnableEngageSystems(true);
+//                //Set Timer
+//                timeToRun = TURN_LEN;
+
+//                StaticUIRefs.Instance.TurnIndicator.color = Color.green;
+
+//                orderPhase = false;
+//            }
+//        }
+//        //If Engage phase
+//        else
+//        {
+//            timeToRun -= SystemAPI.Time.DeltaTime;
+//            StaticUIRefs.Instance.TurnTimer.text = $"0:{(int)timeToRun:D2}";
+//            if (timeToRun <= 0)
+//            {
+//                StaticUIRefs.Instance.TurnTimer.text = "0:00";
+//                //Stop all Engage systems
+//                EnableEngageSystems(false);
+
+//                StaticUIRefs.Instance.TurnIndicator.color = Color.red;
+
+//                orderPhase = true;
+//            }
+//        }
+//    }
+
+//    private void EnableEngageSystems (bool enable)
+//    {
+//        World.Unmanaged.GetExistingSystemState<UnitsSystemGroup>().Enabled = enable;
+//        //World.Unmanaged.GetExistingSystemState<VisionMapSystem>().Enabled = enable;
+//        World.Unmanaged.GetExistingSystemState<AnimationSystem>().Enabled = enable;
+
+//        World.Unmanaged.GetExistingSystemState<TargetingMoveSystem>().Enabled = !enable;
+//        World.Unmanaged.GetExistingSystemState<BasicButtonSystem>().Enabled = !enable;
+//    }
+//}
