3bd527396f3ae8253dc9e2851d0d42698b2b0661
Егор Деркунский <114829512+Palom-Porom@users.noreply.github.com>
2024-05-17 21:22:08 +0300

    Preparations for BuyUnitsSystem

--- FangsAndSteel/Assets/Scripts/General/PrefubsAuthoring.cs
+++ FangsAndSteel/Assets/Scripts/General/PrefubsAuthoring.cs
@@ -14,6 +14,16 @@ public class PrefubsAuthoring : MonoBehaviour
     public GameObject flag7;
     public GameObject flag8;
     public GameObject flag9;
+
+    public GameObject FirstRobot_Red;
+    public GameObject FirstRobot_Blue;
+    public GameObject MachineGunner_Red;
+    public GameObject MachineGunner_Blue;
+    public GameObject AntyTank_Red;
+    public GameObject AntyTank_Blue;
+    public GameObject Tank_Red;
+    public GameObject Tank_Blue;
+
     public class Baker : Baker<PrefubsAuthoring>
     {
         public override void Bake(PrefubsAuthoring authoring)
@@ -30,6 +40,15 @@ public class PrefubsAuthoring : MonoBehaviour
                 flag7 = GetEntity(authoring.flag7, TransformUsageFlags.Dynamic),
                 flag8 = GetEntity(authoring.flag8, TransformUsageFlags.Dynamic),
                 flag9 = GetEntity(authoring.flag9, TransformUsageFlags.Dynamic),
+
+                FirstRobot_Red = GetEntity(authoring.FirstRobot_Red, TransformUsageFlags.Dynamic),
+                FirstRobot_Blue = GetEntity(authoring.FirstRobot_Blue, TransformUsageFlags.Dynamic),
+                MachineGunner_Red = GetEntity(authoring.MachineGunner_Red, TransformUsageFlags.Dynamic),
+                MachineGunner_Blue = GetEntity(authoring.MachineGunner_Blue, TransformUsageFlags.Dynamic),
+                AntyTank_Red = GetEntity(authoring.AntyTank_Red, TransformUsageFlags.Dynamic),
+                AntyTank_Blue = GetEntity(authoring.AntyTank_Blue, TransformUsageFlags.Dynamic),
+                Tank_Red = GetEntity(authoring.Tank_Red, TransformUsageFlags.Dynamic),
+                Tank_Blue = GetEntity(authoring.Tank_Blue, TransformUsageFlags.Dynamic),
             });
             //AddComponent(entity, new SelectTag { selectionRing = GetEntity(authoring.selectionRing, TransformUsageFlags.None) });
         }
@@ -47,6 +66,15 @@ public struct PrefubsComponent : IComponentData
     public Entity flag7;
     public Entity flag8;
     public Entity flag9;
+
+    public Entity FirstRobot_Red;
+    public Entity FirstRobot_Blue;
+    public Entity MachineGunner_Red;
+    public Entity MachineGunner_Blue;
+    public Entity AntyTank_Red;
+    public Entity AntyTank_Blue;
+    public Entity Tank_Red;
+    public Entity Tank_Blue;
 }
 
 public struct FlagTag : IComponentData { }
