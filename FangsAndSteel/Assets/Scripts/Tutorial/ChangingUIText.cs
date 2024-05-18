a8598a9a7f508dd16ac95bec34a90e185f4c280c
Александр Владимирович Дудников <sanya.vd@mail.ru>
2024-05-19 02:16:16 +0300

    Created explanation about movement and enemy position

--- FangsAndSteel/Assets/Scripts/Tutorial/ChangingUIText.cs
+++ FangsAndSteel/Assets/Scripts/Tutorial/ChangingUIText.cs
@@ -25,23 +25,24 @@ public class СhangingTextUI : MonoBehaviour
     public void ButtonClick()
     {
         countclick++;
+
+        bool EventIsTrue = true;
         if (countclick == 1)
         {
             text.text = "Для движения камерой используется сочетание клавиш WASD. \nТак, если Вы нажмете на: \nW, то камера сдвинется вперед\nS, то камера начнет движение назад \nA, то камера будет двигаться влево \nD, то камера сдвинется вправо \nчтобы приблизить поле боя - крутите колесико мышки от себя \nчтобы отдалить - наборот, крутите колесико мышки на себя.";
             textButton.text = "Окей";
         }
         if (countclick == 2)
-        {
-            text.text = " "; //StartCoroutine(HighlightingTheEnemy()); }
-            if (countclick == 3) { text.text = " "; }
-            if (countclick == 4) { text.text = " "; }
-            if (countclick == 5) { text.text = " "; }
-            if (countclick == 6) { text.text = " "; }
-            if (countclick == 7) { text.text = " "; }
-            if (countclick == 8) { text.text = " "; }
-            if (countclick == 9) { text.text = " "; }
-            if (countclick == 10) { text.text = " "; }
-        }
+        { text.text = "Eсли Вы хотите узнать где находятся отряды противника, Вы можете нажать на кнопку \"Показать зону с противниками\". Зная вражеские позиции Вам, бесспорно, легче отправить юнитов к противнику навстречу, для начала срежения. Cоветую так и сделать. \nДля этого выбирите любого Вашего юнита (нажав на него левой кнопкой мыши). Далее нажмите ПКМ по позиции, в которую Вы бы хотели отправить робота."; }
+        if (countclick == 3) { text.text = " "; }
+        if ((countclick == 4) && (EventIsTrue)) { text.text = "  "; }
+        if (countclick == 5) { text.text = " "; }
+        if (countclick == 6) { text.text = " "; }
+        if (countclick == 7) { text.text = " "; }
+        if (countclick == 8) { text.text = " "; }
+        if (countclick == 9) { text.text = " "; }
+        if (countclick == 10) { text.text = " "; }
+        
     }
     /*
     /// <summary>
